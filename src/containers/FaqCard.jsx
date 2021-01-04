ac45e5e5dd899b135304fa6ab8c88b11bc593581
Andres Fernando <andres.fernando08@gmail.com>
2021-01-04 18:00:26 -0500

    [add] responsive media queries

--- src/containers/FaqCard.jsx
+++ src/containers/FaqCard.jsx
@@ -21,18 +21,29 @@ function FaqCard()
     <div className="FaqCard">
       <div className="BoxImage">
         <div className="BoxImage-Inner">
-          <img src={illustrationMobile} alt="" />
-          <img className="BoxImage-Inner-Shadow" src={illustrationBoxShadow} alt="" />
+          <div className="BoxImage-Container">
+            <img className="hidden-lg" src={illustrationMobile} alt="" />
+            <img className="BoxImage-Inner-Shadow hidden-lg" src={illustrationBoxShadow} alt="" />
+            <img className="hidden-xs" src={illustrationDesktop} alt="" />
+          </div>
+        </div>
+        <div className="BoxImage-Shadow-Container">
+          <img className="hidden-xs" src={illustrationBoxShadowDesktop} alt="" />
+        </div>
+        <div className="BoxImage-BoxIcon">
+          <img className="hidden-xs" src={illustrationDesktopBox} alt="" />
         </div>
       </div>
-      <hgroup className="FaqCard-Header">
-        <h1 className="text-center">FAQ</h1>
-      </hgroup>
-      <FaqItemsContainer>
-        {faqItems.map(i => (
-          <FaqItem key={i.id} title={i.title} desc={i.desc} active={i.active} />
-        ))}
-      </FaqItemsContainer>
+      <div className="FaqCard-Container">
+        <hgroup className="FaqCard-Header">
+          <h1>FAQ</h1>
+        </hgroup>
+        <FaqItemsContainer>
+          {faqItems.map(i => (
+            <FaqItem key={i.id} title={i.title} desc={i.desc} active={i.active} />
+          ))}
+        </FaqItemsContainer>
+      </div>
     </div>
   )
 }
